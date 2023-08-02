3e95dc9eb8de33fdc13052112607931a16fa5931
Abhi <aanand@argusoft.com>
2023-08-02 18:00:43 +0530

    cred operation

--- practice.sql
+++ practice.sql
@@ -29,3 +29,40 @@
 --        ('Misty', 'Tabby', 13),
 --        ('George Michael', 'Ragdoll', 9),
 --        ('Jackson', 'Sphynx', 7);
+
+-- Use of where clause
+-- select * from cats where age=4;
+
+-- -- excercise
+-- SELECT cat_id from cats;
+-- SELECT name,breed from cats;
+-- select name ,age from cats WHERE breed="tabby";
+-- SELECT cat_id,age from cats where cat_id=age;
+
+--  Aliases
+-- we can change the column name as required for that particular QUERY
+-- example-select cat_id as id , name from cats;
+
+-- Updating Data
+-- Change tabby cats to shorthair:
+-- UPDATE cats SET breed='Shorthair' WHERE breed='Tabby';
+-- Another update:
+-- UPDATE cats SET age=14 WHERE name='Misty';
+
+-- Update exercise
+-- update cats set name='jack' where name='jackson';
+-- update cats set breed='british shorthair' where name='ringo';
+-- update cats set age=12 where breed='maine coons';
+
+-- Delete all cats with name of 'Egg':
+-- DELETE FROM cats WHERE name='Egg';
+-- -- Delete all rows in the cats table:
+-- DELETE FROM cats;
+
+-- delete exercise
+-- -- Delete all 4 year old cats:
+-- DELETE FROM cats WHERE age=4;
+-- -- Delete cats where cat_id is the same as their age:
+-- DELETE FROM cats WHERE cat_id=age;
+-- -- Delete all cats:
+-- DELETE FROM cats;

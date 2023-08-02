c77788a952a2f1c8fa5553a66cbb58be7fa5099e
Abhi <aanand@argusoft.com>
2023-08-02 17:14:54 +0530

    create and insert

--- practice.sql
+++ practice.sql
@@ -11,3 +11,21 @@
 --     current_status VARCHAR(100) not NULL DEFAULT 'employed',
 --     PRIMARY KEY(id)
 -- );
+
+-- Multiple inserting in the table
+
+-- CREATE TABLE cats (
+--     cat_id INT AUTO_INCREMENT,
+--     name VARCHAR(100),
+--     breed VARCHAR(100),
+--     age INT,
+--     PRIMARY KEY (cat_id)
+-- ); 
+-- INSERT INTO cats(name, breed, age) 
+-- VALUES ('Ringo', 'Tabby', 4),
+--        ('Cindy', 'Maine Coon', 10),
+--        ('Dumbledore', 'Maine Coon', 11),
+--        ('Egg', 'Persian', 4),
+--        ('Misty', 'Tabby', 13),
+--        ('George Michael', 'Ragdoll', 9),
+--        ('Jackson', 'Sphynx', 7);

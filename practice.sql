b3f9117aa173f6c513461851bd942cbb313f5734
Abhi <aanand@argusoft.com>
2023-08-03 15:16:05 +0530

    REFINING SELECTIONS

--- practice.sql
+++ practice.sql
@@ -217,3 +217,97 @@
 --     CONCAT(stock_quantity, ' in stock') AS quantity
 -- FROM
 --     books;
+
+
+
+-- REFINING SELECTIONS:
+-- DISTINCT
+-- SELECT author_lname FROM books;
+-- SELECT DISTINCT author_lname FROM books;
+-- SELECT author_fname, author_lname FROM books;
+-- SELECT DISTINCT CONCAT(author_fname,' ', author_lname) FROM books;
+-- SELECT DISTINCT author_fname, author_lname FROM books;
+
+-- ORDER BY
+-- SELECT * FROM books
+-- ORDER BY author_lname;
+-- SELECT * FROM books
+-- ORDER BY author_lname DESC;
+-- SELECT * FROM books
+-- -- ORDER BY released_year;
+
+-- SELECT book_id, author_fname, author_lname, pages
+-- FROM books ORDER BY 2 desc;
+-- SELECT book_id, author_fname, author_lname, pages
+-- FROM books ORDER BY author_lname, author_fname;
+
+-- LIMIT
+-- SELECT title FROM books LIMIT 3;
+-- SELECT title FROM books LIMIT 1;
+-- SELECT title FROM books LIMIT 10;
+-- SELECT * FROM books LIMIT 1;
+-- SELECT title, released_year FROM books 
+-- ORDER BY released_year DESC LIMIT 5;
+-- SELECT title, released_year FROM books 
+-- ORDER BY released_year DESC LIMIT 1;
+-- SELECT title, released_year FROM books 
+-- ORDER BY released_year DESC LIMIT 14;
+-- SELECT title, released_year FROM books 
+-- ORDER BY released_year DESC LIMIT 0,5;
+-- SELECT title, released_year FROM books 
+-- ORDER BY released_year DESC LIMIT 0,3;
+-- SELECT title, released_year FROM books 
+-- ORDER BY released_year DESC LIMIT 1,3;
+-- SELECT title, released_year FROM books 
+-- ORDER BY released_year DESC LIMIT 10,1;
+-- SELECT * FROM tbl LIMIT 95,18446744073709551615;
+-- SELECT title FROM books LIMIT 5;
+-- SELECT title FROM books LIMIT 5, 123219476457;
+-- SELECT title FROM books LIMIT 5, 50;
+
+-- LIKE
+-- SELECT title, author_fname, author_lname, pages 
+-- FROM books
+-- WHERE author_fname LIKE '%da%';
+-- SELECT title, author_fname, author_lname, pages 
+-- FROM books
+-- WHERE title LIKE '%:%';
+-- SELECT * FROM books
+-- WHERE author_fname LIKE '____';
+-- SELECT * FROM books
+-- WHERE author_fname LIKE '_a_';
+
+-- ESCAPING WILDCARD
+-- To select books with '%' in their title:
+-- SELECT * FROM books
+-- WHERE title LIKE '%\%%';
+-- To select books with an underscore '_' in title:
+-- SELECT * FROM books
+-- WHERE title LIKE '%\_%';
+
+
+
+-- REFINING EXERCISE
+-- SELECT title FROM books WHERE title LIKE '%stories%';
+-- SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
+-- SELECT 
+--     CONCAT(title, ' - ', released_year) AS summary 
+-- FROM books ORDER BY released_year DESC LIMIT 3;
+-- SELECT title, author_lname FROM books WHERE author_lname LIKE '% %';
+-- SELECT title, released_year, stock_quantity 
+-- FROM books ORDER BY stock_quantity LIMIT 3;
+-- SELECT title, author_lname 
+-- FROM books ORDER BY author_lname, title;
+-- SELECT title, author_lname 
+-- FROM books ORDER BY 2,1;
+-- SELECT
+--     CONCAT(
+--         'MY FAVORITE AUTHOR IS ',
+--         UPPER(author_fname),
+--         ' ',
+--         UPPER(author_lname),
+--         '!'
+--     ) AS yell
+-- FROM books ORDER BY author_lname;
+
+

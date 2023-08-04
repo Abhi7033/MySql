a5d8b85c2244435afae2c67870b4c0bc5c1e8fdb
Abhi <aanand@argusoft.com>
2023-08-04 14:52:23 +0530

    CONSTRAINTS AND ALTER TABLE

--- practice.sql
+++ practice.sql
@@ -640,5 +640,75 @@
 -- GROUP BY author_fname, author_lname;
  
 
- 
-    
+
+--  CONSTRAINTS AND ALTER TABLE
+--  CREATE TABLE contacts (
+-- 	name VARCHAR(100) NOT NULL,
+--     phone VARCHAR(15) NOT NULL UNIQUE
+-- );
+-- INSERT INTO contacts (name, phone)
+-- VALUES ('billybob', '8781213455');
+-- This insert would result in an error:
+-- INSERT INTO contacts (name, phone)
+-- VALUES ('billybob', '8781213455');
+
+
+-- CHECK
+-- CREATE TABLE users (
+-- 	username VARCHAR(20) NOT NULL,
+--     age INT CHECK (age > 0)
+-- );
+-- CREATE TABLE palindromes (
+--   word VARCHAR(100) CHECK(REVERSE(word) = word)
+-- )
+
+
+-- NAMED CONSTRAINT
+-- CREATE TABLE users2 (
+--     username VARCHAR(20) NOT NULL,
+--     age INT,
+--     CONSTRAINT age_not_negative CHECK (age >= 0)
+-- );
+-- CREATE TABLE palindromes2 (
+--   word VARCHAR(100),
+--   CONSTRAINT word_is_palindrome CHECK(REVERSE(word) = word)
+-- );
+
+-- MULTIPLE COLUMN CONSTRAINT
+-- CREATE TABLE companies (
+--     name VARCHAR(255) NOT NULL,
+--     address VARCHAR(255) NOT NULL,
+--     CONSTRAINT name_address UNIQUE (name , address)
+-- );
+-- CREATE TABLE houses (
+--   purchase_price INT NOT NULL,
+--   sale_price INT NOT NULL,
+--   CONSTRAINT sprice_gt_pprice CHECK(sale_price >= purchase_price)
+-- );
+
+-- ALTER TABLE - ADD NEW COLUMN
+-- ALTER TABLE companies 
+-- ADD COLUMN phone VARCHAR(15);
+-- ALTER TABLE companies
+-- ADD COLUMN employee_count INT NOT NULL DEFAULT 1;
+
+-- DROP COLUMN FROM TABLE
+-- ALTER TABLE companies DROP COLUMN phone;
+
+-- RENAME TABLE AND COLUMN
+-- RENAME TABLE companies to suppliers;
+-- ALTER TABLE suppliers RENAME TO companies;
+-- ALTER TABLE companies
+-- RENAME COLUMN name TO company_name;
+
+-- MODIFY
+-- ALTER TABLE companies
+-- MODIFY company_name VARCHAR(100) DEFAULT 'unknown';
+-- ALTER TABLE suppliers
+-- CHANGE business biz_name VARCHAR(50);
+
+-- DROP CONSTRAINT AND TABLE CONSTRAINT
+-- ALTER TABLE houses 
+-- ADD CONSTRAINT positive_pprice CHECK (purchase_price >= 0);
+-- ALTER TABLE houses DROP CONSTRAINT positive_pprice;
+

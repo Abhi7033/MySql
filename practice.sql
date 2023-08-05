aabb97f9293eecf1f47d58faf865e23abbef2cfc
Abhi <aanand@argusoft.com>
2023-08-05 18:10:53 +0530

    SQL MODES

--- practice.sql
+++ practice.sql
@@ -1053,6 +1053,73 @@
 --     series ON reviews.series_id = series.id;
 
 
+-- VIEWS
+-- INSTEAD OF TYPING THIS QUERY ALL THE TIME...
+-- SELECT 
+--     title, released_year, genre, rating, first_name, last_name
+-- FROM
+--     reviews
+--         JOIN
+--     series ON series.id = reviews.series_id
+--         JOIN
+--     reviewers ON reviewers.id = reviews.reviewer_id;
+ 
+-- -- WE CAN CREATE A VIEW:
+-- CREATE VIEW full_reviews AS
+-- SELECT title, released_year, genre, rating, first_name, last_name FROM reviews
+-- JOIN series ON series.id = reviews.series_id
+-- JOIN reviewers ON reviewers.id = reviews.reviewer_id;
+ 
+-- NOW WE CAN TREAT THAT VIEW AS A VIRTUAL TABLE 
+-- (AT LEAST WHEN IT COMES TO SELECTING)
+-- SELECT * FROM full_reviews;
+
+-- REPLACING VIEWS
+-- CREATE VIEW ordered_series AS
+-- SELECT * FROM series ORDER BY released_year;
+-- CREATE OR REPLACE VIEW ordered_series AS
+-- SELECT * FROM series ORDER BY released_year DESC;
+-- ALTER VIEW ordered_series AS
+-- SELECT * FROM series ORDER BY released_year;
+-- DROP VIEW ordered_series;
+
+-- HAVING CLAUSE
+-- SELECT 
+--     title, 
+--     AVG(rating),
+--     COUNT(rating) AS review_count
+-- FROM full_reviews 
+-- GROUP BY title HAVING COUNT(rating) > 1;
+
+-- ROLL UP
+-- SELECT 
+--     title, AVG(rating)
+-- FROM
+--     full_reviews
+-- GROUP BY title WITH ROLLUP;
+-- SELECT 
+--     title, COUNT(rating)
+-- FROM
+--     full_reviews
+-- GROUP BY title WITH ROLLUP;
+-- SELECT 
+--     first_name, released_year, genre, AVG(rating)
+-- FROM
+--     full_reviews
+-- GROUP BY released_year , genre , first_name WITH ROLLUP;
+
+-- SQL MODES
+-- -- To View Modes:
+-- SELECT @@GLOBAL.sql_mode;
+-- SELECT @@SESSION.sql_mode;
+-- -- To Set Them:
+-- SET GLOBAL sql_mode = 'modes';
+-- SET SESSION sql_mode = 'modes';
+
+-- SELECT @@GLOBAL.sql_mode;
+-- MODES='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'
+
+
 
 
 
